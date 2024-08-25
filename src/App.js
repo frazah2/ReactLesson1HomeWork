16c07cd1dccba36f6f45ce8b075340c1c305a3e0
“Fizza <email_address>
2024-08-25 18:58:54 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import Header from './components/header/Headrer';
+import Sidebar from './components/sidebar/Sidebar';
+import Maincontent from './components/maincontent/Maincontent';
+import Footer from './components/footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="main1">
+      <div className="main">
+        <div className='main2'>
+         
+        <Header/> 
+        <Sidebar/>
+        <Maincontent/>
+        <Footer/>
+      </div>
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/maincontent/Maincontent.jsx
@@ -0,0 +1,11 @@
+import './Maincontent.css'
+
+const Maincontent = () => {
+    return (
+        <div className="maincontent">
+            <h1>Main Content</h1>
+        </div>
+    )
+}
+
+export default Maincontent;
